a14d975ccef1287387ee54e69041024b7ad69d77
Prin0516 <105021046@live.asia.edu.tw>
2017-10-15 15:52:53 +0800

    test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -33,9 +34,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+        this.primaryStage.getIcons().add(new Image("files:resources/images/if_Address_Book_86957.png"));
         initRootLayout();
         showPersonOverview();
         getPrimaryStage();
+
     }
 
     /**
